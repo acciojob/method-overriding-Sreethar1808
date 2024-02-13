16c2c1658749813a28a2847e2ab682f4257addbd
sreetharraj <sreetharradha188@gmail.com>
2024-02-13 21:08:51 +0530

    fix the issue

---
+++ src/main/java/com/driver/A.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public class A {
+    public static String meth()
+    {
+        return "Invoking method from class A";
+    }
+}
---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public class B {
+    public static String meth()
+    {
+        return "Method is overridden in Extendend class B";
+    }
+}
