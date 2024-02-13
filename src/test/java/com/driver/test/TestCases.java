05fa489a47960f1527ee0e37add650b3aaf2782d
sreetharraj <sreetharradha188@gmail.com>
2024-02-13 22:03:59 +0530

    fix issue 9

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,26 +1,21 @@
 package com.driver.test;
 
+class A {
+    public static String meth() {
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A {
+    public static String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
 
 public class TestCases {
-    class A {
-        public String meth()
-        {
-            return "Invoking method from class A";
-        }
-    }
-    class B extends A{
-        public String meth()
-        {
-            return "Method is overridden in Extendend class B";
-        }
-    }
-    public void main(String[] args) {
-
+    public static void main(String[] args) {
         B b = new B();
         String str = b.meth();
         System.out.println(str);
-
-
     }
-    
 }
