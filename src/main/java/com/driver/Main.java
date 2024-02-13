725430f6ecd96dc0b4378eab7df8cfb515fa3a8f
sreetharraj <sreetharradha188@gmail.com>
2024-02-13 21:35:00 +0530

    fix issue 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,14 @@
 package com.driver;
 
-
+import com.driver.A;
+import com.driver.B;
 public class Main {
     public static void main(String[] args) {
         B b = new B();
         String str = b.meth();
         System.out.println(str);
+
+        String strA = b.meth();
+        System.out.println(strA);
     }
 }
