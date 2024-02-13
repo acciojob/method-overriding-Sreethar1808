936c1f1ebfe0aa82fc082f28170d96605e05f6ef
sreetharraj <sreetharradha188@gmail.com>
2024-02-13 22:36:43 +0530

    fix issue 10

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,8 @@
 package com.driver;
 
-class A {
-    public static String meth() {
-        return "Invoking method from class A";
-    }
-}
 
-class B extends A {
-    public static String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
+
+
 
 public class Main {
     public static void main(String[] args) {
