0d29c27b366be8d82f45d747a60ff59302513c72
sreetharraj <sreetharradha188@gmail.com>
2024-02-16 16:19:23 +0530

    commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,7 +15,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        B b = new B();
-        System.out.println(b.meth());
+        B b1 = new B();
+        b1.meth();
+
+        B b2 = new B();
+        b2.meth();
     }
 }
