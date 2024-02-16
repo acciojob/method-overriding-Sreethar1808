2c145e605d1cec0c67682c26c660667874a3a913
sreetharraj <sreetharradha188@gmail.com>
2024-02-16 14:42:50 +0530

    fix issue 18

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,7 +12,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         B b1 = new B();
         String str = b1.meth();
         System.out.println(str);
